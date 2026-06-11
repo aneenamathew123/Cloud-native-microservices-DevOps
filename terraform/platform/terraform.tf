b7255c1d78ddc88cb823557f0b614a9fce380bcf
aneenamathew123 <aneenamathew123@users.noreply.github.com>
2026-06-11 19:16:37 +0200

    feat: install ArgoCD and configure GitOps platform layer

---
+++ terraform/platform/terraform.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.7"
+    }
+  }
+   backend "s3" {
+    bucket         = "opentelemetry-demo-tfstate"
+    key            = "platform/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraformstate-lock"
+    encrypt        = true
+  }
+}
+
+data "terraform_remote_state" "infra" {
+  backend = "s3"
+  config = {
+    bucket = "opentelemetry-demo-tfstate"
+    key    = "infra/terraform.tfstate"
+    region = "eu-central-1"
+  }
+} 
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+provider "kubectl" {
+  host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.infra.outputs.cluster_ca_certificate)
+ exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name",
+                   data.terraform_remote_state.infra.outputs.cluster_name]
+  }
+ 
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.infra.outputs.cluster_ca_certificate)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name",
+                     data.terraform_remote_state.infra.outputs.cluster_name]
+    }
+  }
+}
